c3ba1f69fcbf2c5f36f315e293ac19bf04450964
YanaNeykova <32368269+YanaNeykova@users.noreply.github.com>
2017-11-05 08:27:51 +0200

    Add files via upload

---
+++ 1. Extract Emails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+
+namespace Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\s([0-9A-Za-z]+(-*|_*|\.*)[0-9a-zA-Z]*@[a-z]+-*[a-z]*(\.[a-z]+)+\b)";
+            string input = Console.ReadLine();
+
+            foreach (Match m in Regex.Matches(input, pattern))
+            {
+                Console.WriteLine(m);
+            }
+        }
+    }
+}
---
+++ T2. Extract Sentences by Keyword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractSentences
+{
+    public class ExtractSentences
+    {
+        public static void Main()
+        {
+            string word = Console.ReadLine();
+            string[] entryText = Console.ReadLine().Split(".!?".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string pattern = $@"\b{word}\b";
+            foreach (var sentence in entryText)
+            {
+                Match match = Regex.Match(sentence, pattern);
+                if (match.Success)
+                {
+                    Console.WriteLine(sentence.Trim());
+                }
+            }
+        }
+    }
+}
